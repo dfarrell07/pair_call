3b6adf78aa1069cf98135e3f568e8f5730b098f3
Daniel Farrell <dfarrell@redhat.com>
2014-08-25 17:34:56 -0400

    Totally changed algo. Two of three methods tested.
    - Use Round Robbin algo (tournament, not scheduler type)
    - Methods covered: compute_pairs, compute_day_diff

--- pair.py
+++ pair.py
@@ -1,37 +1,104 @@
 #!/usr/bin/env python
-""""""
+"""Code for computing team pairs for weekly pair calls."""
 
 import datetime
+from datetime import date, timedelta
 import sys
-
-# If you change the dev list, make sure everyone pulls it
-devs = ["Sam", "Chris", "Dave", "Madhu", "Brent", "Flavio", "Daniel"]
-
-# We started this on 8/13/2014
-start_day = datetime.date(2014, 8, 11)
 
 
 class Pair(object):
 
-    """"""
+    """Computes pair of devs that should meet for the current week.
+
+    Uses a simple round robbin algorithm.
+
+    """
+
+    # If you change the dev list, make sure everyone pulls it
+    devs = ["Sam", "Chris", "Dave", "Madhu", "Brent", "Flavio", "Daniel"]
+
+    # We started this on 8/17/2014
+    start_day = datetime.date(2014, 8, 17)
 
     def __init__(self):
-        """"""
-        week_diff = (datetime.date.today() - start_day).days / 7
+        """Add a placeholder 'dev' if len(devs) is odd."""
+        if len(self.devs) % 2:
+            self.devs.append("Day off")
 
-    def compute_pair(self, usr):
-        """"""
-        # Make sure first letters is cap and rest are lowercase
-        usr = usr.capitalize()
+    def build_fixtures(self):
+        """Build lists of devs rotated around dev at index zero.
 
-        if usr not in devs:
-            err_msg = "Couldn't find {}. Known names: {}".format(usr, devs)
+        Note that these lists are apparently called 'fixtures' in
+        the lingo of tournament bracket builders. I didn't know this
+        as a non-sport person, but it seems pretty well accepted,
+        so I'll honor it.
+
+        """
+        fixture = list(self.devs)
+        fixtures = []
+        for i in range(0, len(self.devs) - 1):
+            fixtures.append(fixture)
+            fixture = [fixture[0]] + [fixture[-1]] + fixture[1:-1]
+
+        return fixtures
+
+    def compute_day_diff(self, start_day, end_day):
+        """Find the number of weekdays between the given dates.
+
+        :param start_day: Temporally first day to find diff between.
+        :type start_day: date
+        :param end_day: Temporally second day to find diff between.
+        :type end_day: date
+
+        """
+        if not start_day <= end_day:
+            err_msg = "Start day ({}) must be before end day ({})".format(
+                start_day, end_day)
             sys.stderr.write(err_msg)
             raise ValueError(err_msg)
+        daygenerator = (start_day + timedelta(x + 1) for x in xrange((
+            end_day - start_day).days))
+        day_diff = sum(1 for day in daygenerator if day.weekday() < 5)
+        return day_diff
+
+    def compute_pairs(self, day=None):
+        """TODO
+
+        TODO: This should take a date
+
+        """
+        # If no day given, use today
+        # Typically true in production, typically pass day when testing
+        if day is None:
+            day = date.today()
+
+        # Find number of weekdays since we started these meetings
+        day_diff = self.compute_day_diff(self.start_day, day)
+
+        # Fixtures are basically rotations of the dev list
+        fixtures = self.build_fixtures()
+        # There are len(devs) fixtures, rotate through them based on day
+        todays_fixture = fixtures[day_diff % len(fixtures)]
+        # Split the fixture in half
+        today_first_half_fix = todays_fixture[:len(todays_fixture) / 2]
+        # Reverse order of second half to finish classic Round Robin structure
+        today_second_half_fix = todays_fixture[len(todays_fixture) / 2:][::-1]
+
+        # Pair off devs in two half fixtures based on indexes
+        pairs = {}
+        for dev0, dev1 in zip(today_first_half_fix, today_second_half_fix):
+            pairs[dev0] = dev1
+            pairs[dev1] = dev0
+        return pairs
+
 
 # When run as script
 if __name__ == "__main__":
-    pair = Pair()
-    print "Your name: ",
-    usr = raw_input()
-    pair.compute_pair(usr)
+    pair_maker = Pair()
+    pairs = pair_maker.compute_pairs()
+    print "Your name:",
+    dev = raw_input()
+    if dev.capitalize() not in pair_maker.devs:
+        print "Unknown dev: {}, known devs: {}".format(dev, pair_maker.devs)
+        sys.exit(1)
+    print "You're paired with: {}".format(pairs[dev])
--- tests/test_pair.py
+++ tests/test_pair.py
@@ -1,17 +1,75 @@
-""""""
+"""Test cases for module that does pair computation."""
 
 import unittest
+from datetime import date, timedelta
+
 import pair as pair_mod
+
 
 class TestComputePair(unittest.TestCase):
 
-    """"""
+    """Tests for method that actually does the pair-selection computation."""
 
     def setUp(self):
-        """"""
-        self.pair = pair_mod.Pair()
+        """Build Pair object."""
+        self.pair_maker = pair_mod.Pair()
 
-    def test_unknown_dev(self):
-        """"""
+    def test_float_day_param(self):
+        """The day param must be type day. Try passing a float."""
+        with self.assertRaises(TypeError):
+            self.pair_maker.compute_pairs(1.)
+
+    def test_never_paired_with_self(self):
+        """Try lots of day_diffs, validating no dev is paired with self."""
+        for day_diff in range(366):
+            day = self.pair_maker.start_day + timedelta(day_diff)
+            pairs = self.pair_maker.compute_pairs(day)
+            for dev0, dev1 in pairs.iteritems():
+                assert dev0 != dev1
+
+    def test_pairwise_pairs(self):
+        """Validate that each member of a pair is paired with the other."""
+        for day_diff in range(366):
+            day = self.pair_maker.start_day + timedelta(day_diff)
+            pairs = self.pair_maker.compute_pairs(day)
+            for dev0, dev1 in pairs.iteritems():
+                assert dev0 == pairs[dev1], "{} != {}, pairs: {}".format(pairs[dev0], pairs[dev1], pairs)
+
+
+class TestComputeDayDiff(unittest.TestCase):
+
+    """Test method that finds difference between two days."""
+
+    def setUp(self):
+        """Build Pair object and example days."""
+        # Days with *0 are one week, days with *1 are the next week
+        self.pair_maker = pair_mod.Pair()
+        self.monday0 = date(2014, 8, 25)
+        self.tuesday0 = date(2014, 8, 26)
+        self.friday0 = date(2014, 8, 29)
+        self.monday1 = date(2014, 9, 1)
+
+    def test_same_day(self):
+        """Test passing start and end days that are equal."""
+        day_diff = self.pair_maker.compute_day_diff(self.monday0, self.monday0)
+        assert day_diff == 0
+
+    def test_today_and_tomorrow(self):
+        """Test start day today and end day tomorrow."""
+        day_diff = self.pair_maker.compute_day_diff(self.monday0, self.tuesday0)
+        assert day_diff == 1
+
+    def test_one_week_diff(self):
+        """Test a Monday and the following Monday."""
+        day_diff = self.pair_maker.compute_day_diff(self.monday0, self.monday1)
+        assert day_diff == 5
+
+    def test_over_weekend(self):
+        """Test a Friday and the next Monday."""
+        day_diff = self.pair_maker.compute_day_diff(self.friday0, self.monday1)
+        assert day_diff == 1
+
+    def test_invalid_order(self):
         with self.assertRaises(ValueError):
-            self.pair.compute_pair("not_a_dev")
+            day_diff = self.pair_maker.compute_day_diff(self.tuesday0,
+                                                        self.monday0)
